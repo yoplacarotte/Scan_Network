8a93674cba799b2ae846b2c95b4838e81bac7b37
benjamingoureau <ben.goureau@hotmail.fr>
2023-05-25 22:26:30 +0200

    update(scan.py): ajout de la fonction ScanNetwork

--- scan.py
+++ scan.py
@@ -1,11 +1,12 @@
-import argparse, socket, os, subprocess, ping3, ipaddress, netifaces
+#!/usr/bin/python3
+import argparse, socket, os, subprocess, ipaddress, netifaces
 from smb.SMBConnection import SMBConnection
 
 
-def getNetwork():
-    ### Grab the network ip
-    ### INPUT = NOTHING
-    ### OUTPUT = string
+def GetNetwork():
+    # Get the network address
+    # INPUT = NOTHING
+    # OUTPUT = string : 192.168.1.0/24
     
     iface = netifaces.gateways()
     info = netifaces.ifaddresses(iface['default'][netifaces.AF_INET][1])
@@ -14,6 +15,19 @@ def getNetwork():
     netmask = info['netmask']
     netmask_wildcard = str(ipaddress.IPv4Address(int(ipaddress.IPv4Address(netmask))^(2**32-1)))
     CIDR = ipaddress.IPv4Address._prefix_from_ip_int(int(ipaddress.IPv4Address(netmask_wildcard))^(2**32-1))
-    return str(addr) +"/"+ str(CIDR)
+    return ipaddress.IPv4Network(addr +"/"+ str(CIDR), strict=False)
 
-print(getNetwork())
+def ScanNetwork():
+    # Check if the host is up !
+    # INPUT = ip : 192.168.1.1
+    # OUTPUT = string : Host Unreachable or Host 192.168.1.1 Up !
+    
+    ipNetwork = GetNetwork()
+    for ip in ipNetwork:
+        ipsplit = str(ip)
+        tmpsplit = ipsplit.split(".")
+        if tmpsplit[3] != "0" and tmpsplit[3] != "255":
+            print(ip)
+
+##MAIN##
+ScanNetwork()
